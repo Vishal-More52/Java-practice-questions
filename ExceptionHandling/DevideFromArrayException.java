c12b6999bdfb7e5119b1711e3e2dbb701679fbb0
Vishal More <vishalmore4821@gmail.com>
2026-04-04 21:45:52 +0530

    Divide from array and handle exception

---
+++ ExceptionHandling/DevideFromArrayException.java
@@ -0,0 +1,17 @@
+package ExceptionHandling;
+
+public class DevideFromArrayException {
+  public static void main(String[] args) {
+    int[] numerators = {10 , 200 , 30, 40 };
+    int[] denominators = {1,2,0,4};
+    
+    for(int i = 0; i< numerators.length; i++){
+      System.out.println(divide(numerators[i],denominators[i]));
+    }
+    System.out.println("Good Job , Code is runnig👍🏼");
+  }
+
+  public static  int divide(int a,int b){
+     return a/b;
+  }
+}
