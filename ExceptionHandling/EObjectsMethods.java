2630857b0321b6d3e39f3650ba48a7cd788f74e8
Vishal More <vishalmore4821@gmail.com>
2026-04-05 21:36:54 +0530

    Methods of e object

---
+++ ExceptionHandling/EObjectsMethods.java
@@ -0,0 +1,25 @@
+package ExceptionHandling;
+
+public class EObjectsMethods {
+ 
+  public static void main(String[] args) {
+    
+    int a[] = new int[5];
+    System.out.println("Hello guys");
+
+    try {
+      int result = 5/0;
+
+      System.out.println(a[8]);
+    } catch (ArrayIndexOutOfBoundsException e) {
+      System.out.println("Tried to access the out of bound element");
+    }catch(ArithmeticException e){
+      System.out.println(e.getStackTrace());
+      System.out.println(e.getMessage());
+      System.out.println(e);
+    }
+
+    System.out.println("Buy guys");
+  }
+
+}
