fed206eb0c455674ab61a8883a7b9f784f2db3d4
Vishal More <vishalmore4821@gmail.com>
2026-04-10 22:47:50 +0530

    Adding TreeMap

---
+++ Collection_Framework/Map/TreeMapExample.java
@@ -0,0 +1,36 @@
+package Collection_Framework.Map;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class TreeMapExample {
+  public static void main(String[] args) {
+    Map<String,Integer> scores = new TreeMap<>();
+
+    //adding elements
+    scores.put("Zebra", 10);
+    scores.put("Apple", 50);
+    scores.put("Mango", 30);
+    scores.put("Banana", 25);
+
+    //key will be sorted in natural order (alphabetical order for strings)
+    System.out.println("Scores in sorted order: " + scores);
+
+    // adding null key will throw NullPointerException
+    try {
+      scores.put(null, 0);
+      scores.put(null, 5);
+    
+    } catch (NullPointerException e) {
+      System.out.println("Cannot add null key to TreeMap: " + e.getMessage());
+    }
+
+    // adding null value is allowed
+      scores.put("Grapes",null);
+      scores.put("Grapes", null);
+      scores.put("Chiku", null);
+          
+      System.out.println("Scores after adding null values: " + scores);
+    
+  }
+}
