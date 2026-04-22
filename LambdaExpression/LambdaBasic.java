3ab55a495cedb9d40372bd5bd76497b2016bd15d
Vishal More <vishalmore4821@gmail.com>
2026-04-22 14:23:59 +0530

    Lambda with parameters

--- LambdaExpression/LambdaBasic.java
+++ LambdaExpression/LambdaBasic.java
@@ -20,6 +20,7 @@ public class LambdaBasic {
   }
 }
 
+@FunctionalInterface
 interface SayHello{
   void speak();
 }
---
+++ LambdaExpression/LambdaParameters.java
@@ -0,0 +1,18 @@
+package LambdaExpression;
+
+@FunctionalInterface
+interface Walker {
+  int walk(int steps);
+}
+
+public class LambdaParameters {
+  public static void main(String[] args) {
+    Walker walker = (steps) -> {
+      System.out.println("Walking " + steps + " steps...");
+      return steps * 10;// returns calories burned
+    };
+
+    int calories = walker.walk(5);
+    System.out.println("Calories burned: " + calories);
+  }
+}
