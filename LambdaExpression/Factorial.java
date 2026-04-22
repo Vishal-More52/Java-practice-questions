c28970957b58ddab786cb9179b4ccaf4438e5497
Vishal More <vishalmore4821@gmail.com>
2026-04-22 14:36:57 +0530

    Factorial using lambda

---
+++ LambdaExpression/Factorial.java
@@ -0,0 +1,15 @@
+package LambdaExpression;
+
+import java.util.function.LongUnaryOperator;
+import java.util.stream.LongStream;
+
+public class Factorial {
+  public static void main(String[] args) {
+    
+    //lambda to calculate factorial
+    LongUnaryOperator factorial = n -> LongStream.rangeClosed(1, n).reduce(1, (a,b) -> a * b);
+
+
+    System.out.println("Factorial of 5: " + factorial.applyAsLong(5));
+  }
+}
