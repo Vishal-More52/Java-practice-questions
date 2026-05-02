b8f87a8d52b5e6a0da67f6314e7ff072cf5219ad
Vishal More <vishalmore4821@gmail.com>
2026-05-02 19:33:11 +0530

    Adding thread Scheduler

---
+++ Threading/ThreadScheduler1.java
@@ -0,0 +1,24 @@
+package Threading;
+
+class ThreadScheduler extends Thread{
+
+    public void run(){
+
+    String n = Thread.currentThread().getName();
+    for(int i =1 ; i<=3; i++){
+        System.out.println(n);
+    }
+        
+    }
+}
+public class ThreadScheduler1 {
+    public static void main(String[] args) {
+        ThreadScheduler t1 = new ThreadScheduler();
+        ThreadScheduler t2 = new ThreadScheduler();
+        ThreadScheduler t3 = new ThreadScheduler();
+
+        t1.setName("Thread 1");
+        t2.setName("Thread 2");
+        t3.setName("Thread 3");
+    }
+}
