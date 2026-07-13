0288be024f78b54d687616437ac4e96eb082a3ac
Vishal More <vishalmore4821@gmail.com>
2026-07-13 12:58:01 +0530

    Sum of N Natural numbers

---
+++ java logic question/SumOf_N_Numbers.java
@@ -0,0 +1,17 @@
+public class SumOf_N_Numbers {
+
+    static int findSum(int n){
+        int sum = 0;
+        for(int i = 1 ; i <=n ; i++)
+        {
+            sum = sum + i;
+
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(findSum(n));
+    }
+}
